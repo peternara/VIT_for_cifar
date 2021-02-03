b87623cfbdc08539f95914bea1e53fec38607f71
thejiangcj <59393870+thejiangcj@users.noreply.github.com>
2021-02-03 20:07:55 +0800

    1

--- vit/models/ViT.py
+++ vit/models/ViT.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from Transformer import TransformerModel
+from vit.models.Transformer import TransformerModel
 from PositionalEncoding import (
     FixedPositionalEncoding,
     LearnedPositionalEncoding,
