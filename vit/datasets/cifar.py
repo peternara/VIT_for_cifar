e88638bed7b58333b23358b8915ec5ad0d6a10bc
thejiangcj <59393870+thejiangcj@users.noreply.github.com>
2021-02-08 19:50:59 +0800

    u3

--- vit/datasets/cifar.py
+++ vit/datasets/cifar.py
@@ -10,13 +10,11 @@ from torch.utils.data import (
 
 
 def cifar(args, distributed=False):
-    if args.local_rank not in [-1, 0]:
-        torch.distributed.barrier()
 
     transform_train = transforms.Compose(
         [
             transforms.RandomResizedCrop(
-                (args.img_size, args.img_size), scale=(0.05, 1.0)
+                (28, 28), scale=(0.05, 1.0)
             ),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
@@ -24,7 +22,7 @@ def cifar(args, distributed=False):
     )
     transform_test = transforms.Compose(
         [
-            transforms.Resize((args.img_size, args.img_size)),
+            transforms.Resize((28, 28)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
         ]
@@ -41,8 +39,6 @@ def cifar(args, distributed=False):
                 download=True,
                 transform=transform_test,
             )
-            if args.local_rank in [-1, 0]
-            else None
         )
 
     else:
@@ -56,22 +52,17 @@ def cifar(args, distributed=False):
                 download=True,
                 transform=transform_test,
             )
-            if args.local_rank in [-1, 0]
-            else None
         )
-    if args.local_rank == 0:
-        torch.distributed.barrier()
+
 
     train_sampler = (
         RandomSampler(trainset)
-        if args.local_rank == -1
-        else DistributedSampler(trainset)
     )
     test_sampler = SequentialSampler(testset)
     train_loader = DataLoader(
         trainset,
         sampler=train_sampler,
-        batch_size=args.train_batch_size,
+        batch_size=args.batch_size,
         num_workers=4,
         pin_memory=True,
     )
@@ -79,7 +70,7 @@ def cifar(args, distributed=False):
         DataLoader(
             testset,
             sampler=test_sampler,
-            batch_size=args.eval_batch_size,
+            batch_size=args.val_batch_size,
             num_workers=4,
             pin_memory=True,
         )
@@ -87,4 +78,4 @@ def cifar(args, distributed=False):
         else None
     )
 
-    return train_loader, test_loader
+    return train_loader, '_', test_loader, '_'
